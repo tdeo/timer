401668d0a69cae5417faa45444c19d03beaa255a
Thierry Deo <thierry.deo@gmail.com>
2018-06-13 17:57:57 +0200

    Use bootstrap

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
 
 function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
+  win = new BrowserWindow({width: 500, height: 300});
   win.loadFile('index.html')
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
