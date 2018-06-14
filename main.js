7396f402eff91891a115510979d67cb669a4342d
Thierry Deo <thierry.deo@gmail.com>
2018-06-14 09:11:10 +0200

    Add last times

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const {app, BrowserWindow} = require('electron');
+const {app, globalShortcut, BrowserWindow} = require('electron');
 
 function createWindow () {
-  win = new BrowserWindow({width: 500, height: 300});
-  win.loadFile('index.html')
+  win = new BrowserWindow({width: 500, height: 170, nodeIntegration: false});
+  win.setMenu(null);
+  win.loadFile('index.html');
+  globalShortcut.register('CmdOrCtrl+/', () => {
+    win.webContents.executeJavaScript("add();");
+  });
 }
 
 app.on('ready', createWindow);
--- timer.js
+++ timer.js
@@ -3,12 +3,33 @@ var startedAt;
 var pausedTime;
 var pausedAt;
 
+function readableTime(time) {
+  var millis = Math.round(time % 1000 / 100) / 10;
+  var seconds = Math.floor(time / 1000);
+  var minutes = Math.floor(seconds / 60);
+  seconds = seconds % 60;
+  var hours = Math.floor(minutes / 60);
+  minutes = minutes % 60;
+
+  var result = seconds + millis;
+  if (minutes > 0 || hours > 0) {
+    if (seconds < 10 ) { result = "0" + result; }
+    result = minutes + ":" + result;
+  }
+  if (hours > 0) {
+    if (minutes < 10) { result = "0" + result; }
+    result = hours + ":" + result;
+  }
+  return result
+}
+
 function init() {
   splits = [];
   startedAt = Date.now();
   pausedTime = 0;
   pausedAt = undefined;
   redraw();
+  $("button").css("display", "initial");
 }
 
 function runtime() {
@@ -16,8 +37,25 @@ function runtime() {
 }
 
 function redraw() {
-  console.log(splits);
-  document.getElementById('counter').innerHTML = (isPaused() ? "Paused - " : "") + splits.length;
+  $("#counter")[0].innerHTML = splits.length;
+
+  if (isPaused()) {
+    $(".btn.btn-secondary").addClass("btn-warning");
+  } else {
+    $(".btn.btn-secondary").removeClass("btn-warning");
+  }
+
+  var table = "";
+  var previous = splits[splits.length - 6];
+  if (previous === undefined) { previous = 0; }
+  splits.slice(-5).forEach(function(value, idx) {
+    row = "<tr><td>" + (Math.max(splits.length - 4, 1) + idx) + "</td><td>" + readableTime(value) + "</td><td>";
+    if (previous !== undefined) { row += readableTime(value - previous); }
+    row += "</td></tr>";
+    previous = value;
+    table = row + table;
+  });
+  $("#lastSplits")[0].innerHTML = table;
 }
 
 function add() {
